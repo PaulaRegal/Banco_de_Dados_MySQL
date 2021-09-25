71696047283f0612d079eae004c417aecd02059d
Paula Regal <paulacregal@yahoo.com.br>
2021-09-25 17:02:40 -0300

    insert into, if not exist03 insert into, if not exist.sql

---
+++ 03 insert into, if not exist.sql
@@ -0,0 +1,29 @@
+create table if not exists cursos (
+nome varchar(30) not null unique,
+descricao text,
+carga int unsigned,
+totaulas int unsigned,
+ano year default '2016'
+) default charset=utf8;
+
+describe cursos;
+
+alter table cursos
+add column idcurso int first;
+
+alter table cursos
+add primary key (idcurso);
+
+
+create table if not exists teste(
+id int,
+nome varchar(10),
+idade int
+);
+
+insert into teste value
+('1', 'Pedro', '22');
+
+select * from teste;
+
+drop table if exists teste;
